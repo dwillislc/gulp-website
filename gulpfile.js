114c671fcb677ac7a73596886ef3c315cf3e84b7
David Willis <dwillislc@gmail.com>
2019-02-24 15:38:04 -0500

    concat all js files into app.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-var x = "Branches with different text?"; 
+var gulp = require('gulp'); 
+var concat = require('gulp-concat'); 
+
+gulp.task('concatScripts', function() {
+	gulp.src(['js/circle/autogrow.js',
+			'js/circle/circle.js', 
+			'js/global.js'
+		]).pipe(concat('app.js')).pipe(gulp.dest('js'));
+});
+
