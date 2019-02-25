286d655f0ca493edaf0051218b62877476614389
David Willis <dwillislc@gmail.com>
2019-02-25 17:54:56 -0500

    minify app.js file and store in dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'); 
 var concat = require('gulp-concat'); 
+var uglify = require('gulp-uglify'); 
+var rename = require('gulp-rename'); 
 
 // Concat all js files into app.js 
 gulp.task('concatScripts', function() {
@@ -9,3 +11,10 @@ gulp.task('concatScripts', function() {
 		]).pipe(concat('app.js')).pipe(gulp.dest('js'));
 });
 
+// Minify all js files and insert into all.min.js
+gulp.task('minifyScripts', function () {
+	gulp.src('js/app.js')
+	.pipe(uglify())
+	.pipe(rename('all.min.js'))
+	.pipe(gulp.dest('dist/scripts'));
+}) 
